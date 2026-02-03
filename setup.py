bab04ee3655dce77b754bb096cda9a0defdc1d28
Denys Fedoryshchenko <denys.f@collabora.com>
2026-02-03 08:31:39 +0200

    deps: use mainstream jq package
    Signed-off-by: Denys Fedoryshchenko <denys.f@collabora.com>

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setuptools.setup(
         "python-dateutil",
         "cached-property",
         "kcidb-io@git+https://github.com/kernelci/kcidb-io.git",
-        "jq@git+https://github.com/kernelci/jq.py.git@1.7.0.post1",
+        "jq",
     ],
     extras_require=dict(
         dev=[
